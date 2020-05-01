8b722d3c104993f93a5c0c1f36ef9703a2b1d6a5
Jason Aviles <sandman2k18@outlook.com>
2020-05-01 16:08:52 -0400

    update

--- index.js
+++ index.js
@@ -22,46 +22,46 @@ app.get('/', (req, res) => {
   res.send('Welcome to my api');
 })
 
-app.post('/api/v1', (req,res) => {
-  var data = req.body;
+// app.post('/api/v1', (req,res) => {
+//   var data = req.body;
 
-var smtpTransport = nodemailer.createTransport({
+// var smtpTransport = nodemailer.createTransport({
 
  
-  host: "smtp-mail.outlook.com",
-  secureConnection: false, 
-  port: 587, 
-  auth: {
-    user: 'info@jeffaviles.com',
-    pass: 'Jeter2015.'
-  },
-  tls: {
-      ciphers:'SSLv3'
-  }
+//   host: "smtp-mail.outlook.com",
+//   secureConnection: false, 
+//   port: 587, 
+//   auth: {
+//     user: 'info@jeffaviles.com',
+//     pass: 'Jeter2015.'
+//   },
+//   tls: {
+//       ciphers:'SSLv3'
+//   }
 
-});
+// });
 
-var mailOptions = {
-  from: 'info@jeffaviles.com',
-  to: data.email,
-  subject: `The Overthinking Cure    ${data.username} you have just received your free pdf` ,
-  html: "<b>Hello world?</b>"
-           ,
-          attachments: [{
-            filename: 'The_Overthinking_Cure.pdf',
-            path: './output/The_Overthinking_Cure.pdf',
-            contentType: 'application/pdf'
-          }]
-};
+// var mailOptions = {
+//   from: 'info@jeffaviles.com',
+//   to: data.email,
+//   subject: `The Overthinking Cure    ${data.username} you have just received your free pdf` ,
+//   html: "<b>Hello world?</b>"
+//            ,
+//           attachments: [{
+//             filename: 'The_Overthinking_Cure.pdf',
+//             path: './output/The_Overthinking_Cure.pdf',
+//             contentType: 'application/pdf'
+//           }]
+// };
 
-smtpTransport.sendMail(mailOptions,
-(error, response) => {
-  if(error) {
-    res.send(error)
-  }else {
-    res.send('Success')
-  }
-  smtpTransport.close();
-});
+// smtpTransport.sendMail(mailOptions,
+// (error, response) => {
+//   if(error) {
+//     res.send(error)
+//   }else {
+//     res.send('Success')
+//   }
+//   smtpTransport.close();
+// });
 
-})
+// })
